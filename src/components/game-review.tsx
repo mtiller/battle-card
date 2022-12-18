3c89e7e8c405861e93c5607de1ca75c7a54d0235
Michael Tiller <michael.tiller@gmail.com>
2022-12-18 16:16:40 -0500

    feat: added filtering to game review

--- src/components/game-review.tsx
+++ src/components/game-review.tsx
@@ -1,4 +1,4 @@
-import { Button, Group, Modal, Slider } from "@mantine/core";
+import { Button, Group, Modal, SegmentedControl, Slider } from "@mantine/core";
 import React from "react";
 import { Outcome } from "../rules";
 import { GameTimeline } from "./game-timeline";
@@ -10,9 +10,8 @@ export interface GameReviewProps {
 
 export const GameReview = (props: GameReviewProps) => {
   const [current, setCurrent] = React.useState(0);
-  const [opened, setOpened] = React.useState(false);
-
-  const { results, seed } = props;
+  const [filtering, setFiltering] = React.useState("all");
+  const results = ofInterest(props.results, filtering);
   return (
     <div>
       <div>
@@ -21,6 +20,16 @@ export const GameReview = (props: GameReviewProps) => {
             <p>No simulation results to show yet.</p>
           ) : (
             <div>
+              <SegmentedControl
+                onChange={(ev) => setFiltering(ev)}
+                value={filtering}
+                data={[
+                  { label: "All", value: "all" },
+                  { label: "Wins", value: "wins" },
+                  { label: "Losses", value: "losses" },
+                  { label: "Lost Zone 1", value: "lzone1" },
+                ]}
+              />
               <Slider
                 title="Select Game"
                 min={1}
@@ -29,8 +38,8 @@ export const GameReview = (props: GameReviewProps) => {
                 max={results.length}
                 style={{ marginTop: 10, marginBottom: 5 }}
                 marks={[
-                  { value: 0, label: "first" },
-                  { value: results.length, label: "last" },
+                  { value: 0, label: "1" },
+                  { value: results.length, label: `${results.length}` },
                 ]}
               />
             </div>
@@ -42,3 +51,19 @@ export const GameReview = (props: GameReviewProps) => {
     </div>
   );
 };
+
+function ofInterest(results: Outcome[], filtering: string) {
+  switch (filtering) {
+    case "wins":
+      return results.filter((r) => r.final.outcome === "won");
+    case "losses":
+      return results.filter((r) => r.final.outcome === "lost");
+    case "lzone1":
+      return results.filter(
+        (r) => r.history[0].afterBattle.zones[0].control === "german"
+      );
+    case "all":
+    default:
+      return results;
+  }
+}
