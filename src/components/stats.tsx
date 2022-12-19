dc536a12ae438fc25b28bdc581934dfa7843a8ec
Michael Tiller <michael.tiller@gmail.com>
2022-12-18 22:53:47 -0500

    feat: status on outcome timing

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,3 +1,4 @@
+import { RingProgress, Text } from "@mantine/core";
 import { Statistics } from "../hooks/stats";
 import { WinRing } from "./win-ring";
 
@@ -5,13 +6,50 @@ export interface StatsProps {
   stats: Statistics;
 }
 
+const colors = [
+  "cyan",
+  "red",
+  "magenta",
+  "purple",
+  "blue",
+  "green",
+  "orange",
+  "gray",
+];
 export const Stats = (props: StatsProps) => {
+  const entries = [...props.stats.histo.entries()];
+  const sections = entries.map((e, i) => ({
+    value: (e[1] * 100) / props.stats.results.length,
+    color: colors[i],
+    tooltip: `${e[0]} - ${(e[1] * 100) / props.stats.results.length}%`,
+  }));
   return (
     <div>
       <h3>Stats</h3>
-      <div style={{ display: "flex", flexDirection: "column" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          maxWidth: "50vw",
+        }}
+      >
         <WinRing title="Win Rate" per={+props.stats.winPer} />
         <WinRing title="German Control" per={+props.stats.davidPer} />
+        <div>
+          <RingProgress
+            size={150}
+            thickness={20}
+            label={
+              <div>
+                <Text align="center" size="md">
+                  Timing
+                </Text>
+              </div>
+            }
+            sections={sections}
+          />
+        </div>
       </div>
     </div>
   );
--- src/hooks/stats.ts
+++ src/hooks/stats.ts
@@ -6,6 +6,17 @@ export function useStats(results: Outcome[]) {
   const won = wins.length;
   const lost = losses.length;
   const winPer = ((won * 100) / results.length).toFixed(2);
+  const histo = new Map<string, number>();
+
+  for (let i = 0; i < results.length; i++) {
+    const result = results[i];
+    const key = `${result.final.outcome} on day ${result.final.day}`;
+    histo.set(key, (histo.get(key) ?? 0) + 1);
+  }
+  const days = results.map((r) => ({
+    outcome: r.final.outcome,
+    days: r.final.day,
+  }));
 
   const alliedSetback = results.filter(
     (r) => r.history[0].afterBattle.zones[0].control === "german"
@@ -14,7 +25,18 @@ export function useStats(results: Outcome[]) {
 
   const davidPer = ((100 * stillWon.length) / alliedSetback.length).toFixed(2);
   console.log("Stats recomputed");
-  return { wins, losses, won, lost, winPer, davidPer, alliedSetback, stillWon };
+  return {
+    results,
+    wins,
+    losses,
+    won,
+    lost,
+    winPer,
+    davidPer,
+    alliedSetback,
+    stillWon,
+    histo,
+  };
 }
 
 export type Statistics = ReturnType<typeof useStats>;
